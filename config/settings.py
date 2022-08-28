de5bee0adcedd0d7d9b63cc068ddf5b915b3b90f
KimMinGyu <akdrb159@naver.com>
2022-08-28 15:56:28 +0900

    DB 연동

--- config/settings.py
+++ config/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': { 
+    	'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'ot', 
+        'USER': 'root', 
+        'PASSWORD': 'rla!57752300', 
+        'HOST': 'localhost', 
+        'PORT': '3306', 
+     } 
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -103,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
