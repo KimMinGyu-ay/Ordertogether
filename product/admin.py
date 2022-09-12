52bb5c1d85de94b2f2ca907225982c572db5c24c
KimMinGyu <akdrb159@naver.com>
2022-09-12 16:32:23 +0900

    회의 업데이트

--- product/admin.py
+++ product/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Product_list
+from .models import Product_list,Product_detail
 # Register your models here.
 admin.site.register(Product_list)
+admin.site.register(Product_detail)
--- product/views.py
+++ product/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from .models import Product_list
+from .models import Product_list,Product_detail
 # Create your views here.
 
 
 def product_list(request):
     goods = Product_list.objects.all().order_by("pk")
+    products = Product_detail.objects.all().order_by("pk")
     print(goods)
     return render(
         request,"product/product_list.html",
         {
-            'goods' : goods 
+            'goods' : goods ,
+            'products': products
         }
     )
